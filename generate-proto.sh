55daea689f6ac638e945a1169a56b8bbb0879b34
Jay Lee <busyjaylee@gmail.com>
2018-06-08 19:57:24 +0800

    Use the old way to read enum

---
+++ generate-proto.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+protoc proto/eraftpb.proto --rust_out=src/
+# TODO: remove this once stepancheg/rust-protobuf#233 is resolved.
+sed -i -E "s/::protobuf::rt::read_proto3_enum_with_unknown_fields_into\(([^,]+), ([^,]+), &mut ([^,]+), .*$/if \1 == ::protobuf::wire_format::WireTypeVarint \{\3 = \2.read_enum()?;\} else \{ return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type)); \}/" src/eraftpb.rs
