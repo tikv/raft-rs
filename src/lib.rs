311b3b53ba8e21a60d2cf00dfeeb024cfed1d679
Ana Hobden <operator@hoverbear.org>
2019-02-11 10:05:32 -0800

    Fix feature so it functions on stable (#180)

--- src/lib.rs
+++ src/lib.rs
@@ -260,7 +260,7 @@ For more information, check out an [example](examples/single_mem_node/main.rs#L1
 
 */
 
-#![cfg_attr(not(feature = "cargo-clippy"), allow(unknown_lints))]
+#![deny(clippy::all)]
 #![deny(missing_docs)]
 
 #[cfg(feature = "failpoint")]
