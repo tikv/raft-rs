c9ddf23c63be2948423bbabe963d1182f3afc3a3
ice1000 <ice1000kotlin@foxmail.com>
2019-04-19 00:17:14 -0400

    Remove the whole `cfg_attr`
    Signed-off-by: ice1000 <ice1000kotlin@foxmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -261,7 +261,6 @@ For more information, check out an [example](examples/single_mem_node/main.rs#L1
 */
 
 #![cfg_attr(not(feature = "cargo-clippy"), allow(unknown_lints))]
-#![cfg_attr(feature = "cargo-clippy")]
 #![deny(missing_docs)]
 
 #[cfg(feature = "failpoint")]
