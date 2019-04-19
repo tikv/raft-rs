75370ec804aa20d03e59ffa8377343f39ce418a8
ice1000 <ice1000kotlin@foxmail.com>
2019-04-18 23:53:13 -0400

    Address comments: add `Codec` back, replace protobuf version with 2
    Signed-off-by: ice1000 <ice1000kotlin@foxmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 use prost::{DecodeError, EncodeError};
+use protobuf::ProtobufError;
 use std::error;
 use std::{cmp, io, result};
 
@@ -46,6 +47,13 @@ quick_error! {
         /// The configuration is invalid.
         ConfigInvalid(desc: String) {
             description(desc)
+        }
+        /// A Protobuf message failed in some manner.
+        Codec(err: ProtobufError) {
+            from()
+            cause(err)
+            description(err.description())
+            display("prost encode error {:?}", err)
         }
         /// Encode error from prost
         ProstEncode(err: EncodeError) {
