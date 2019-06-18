4892b52a06315f83580e8716adcec4db230197ff
Nick Cameron <nrc@ncameron.org>
2019-06-18 18:10:36 +1200

    Move protos to a separate crate
    Makes Raft a workspace project
    
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- proto/build.rs
+++ proto/build.rs
@@ -1,15 +1,4 @@
-// Copyright 2019 PingCAP, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use protobuf_build::*;
 use std::fs::{read_dir, File};
@@ -44,7 +33,7 @@ fn main() {
         println!("cargo:rerun-if-changed={}", f);
     }
 
-    // Generate Prost files.
+    // Generate Prost output.
     generate_prost_files(&file_names, "src/prost");
     let mod_names = module_names_for_dir("src/prost");
     generate_wrappers(
