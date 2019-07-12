06dc757971c0a54441fea305e24429310a6fade3
Jay Lee <busyjaylee@gmail.com>
2019-07-13 05:39:06 +0800

    make ci test prost features

--- proto/build.rs
+++ proto/build.rs
@@ -1,5 +1,8 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 fn main() {
-    jinkela_build::Builder::default().include_dir("proto").compile_proto("proto/eraftpb.proto").build();
+    jinkela_build::Builder::default()
+        .include_dir("proto")
+        .compile_proto("proto/eraftpb.proto")
+        .build();
 }
