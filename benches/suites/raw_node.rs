f7f7d0121132ef6406b59fc774778285cd1477d6
Ana Hobden <operator@hoverbear.org>
2019-12-02 23:43:49 -0800

    Update copyright header (#322)
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- benches/suites/raw_node.rs
+++ benches/suites/raw_node.rs
@@ -1,3 +1,5 @@
+// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
+
 use criterion::{BatchSize, Bencher, BenchmarkId, Criterion, Throughput};
 use raft::eraftpb::{ConfState, Entry, Message, Snapshot, SnapshotMetadata};
 use raft::{storage::MemStorage, Config, RawNode};
--- src/storage.rs
+++ src/storage.rs
@@ -4,18 +4,7 @@
 //! Custom implementations of this are normal and this is likely to be a key integration
 //! point for your distributed storage.
 
-// Copyright 2016 PingCAP, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 // Copyright 2015 The etcd Authors
 //
