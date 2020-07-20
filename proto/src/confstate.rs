075ad9a1a5b53b030eb12a3155afdb680f86a2fa
Jay Lee <BusyJayLee@gmail.com>
2020-07-20 13:31:33 +0800

    address comment
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- proto/src/confstate.rs
+++ proto/src/confstate.rs
@@ -19,7 +19,10 @@ fn eq_without_order(lhs: &[u64], rhs: &[u64]) -> bool {
 // Returns true if the inputs describe the same configuration.
 #[must_use]
 pub fn conf_state_eq(lhs: &ConfState, rhs: &ConfState) -> bool {
-    // In most cases, the orders are the same.
+    // The orders are different only when hash algorithm or insert orders are
+    // different. In most case, only one hash algorithm is used. Insert orders
+    // should be the same due to the raft protocol. So in most case, they can
+    // be compared directly.
     if lhs.get_voters() == rhs.get_voters()
         && lhs.get_learners() == rhs.get_learners()
         && lhs.get_voters_outgoing() == rhs.get_voters_outgoing()
