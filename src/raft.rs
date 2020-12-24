3bd71b678f35bd4468d67a0f36a6a6d2049ef0c0
Jay Lee <BusyJayLee@gmail.com>
2020-12-24 12:42:21 +0800

    address comment
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- src/raft.rs
+++ src/raft.rs
@@ -1432,11 +1432,7 @@ impl<T: Storage> Raft<T> {
                     to_send.commit = commit;
                     to_send.commit_term = commit_term;
                     self.r.send(to_send, &mut self.msgs);
-                    // It should be safe to commit log by vote in all conditions. Here
-                    // use a stricter condition for best safety.
-                    if self.leader_id == INVALID_ID || !self.promotable {
-                        self.maybe_commit_by_vote(&m);
-                    }
+                    self.maybe_commit_by_vote(&m);
                 }
             }
             _ => match self.state {
