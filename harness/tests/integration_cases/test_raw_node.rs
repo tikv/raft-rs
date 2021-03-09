tikv/raft-rs

commit a45c4a33229090a0984638bfa4bc163ef8b284ea

Change the behavior of persisted index for snapshot (#410)
Signed-off-by: gengliqi <gengliqiii@gmail.com>

harness/src/interface.rs
@@ -60,7 +60,9 @@ impl Interface {
                 let snap = snapshot.clone();
                 self.raft_log.stable_snap();
                 let index = snap.get_metadata().index;
+                let term = snap.get_metadata().term;
                 self.mut_store().wl().apply_snapshot(snap).expect("");
+                self.on_persist_entries(index, term);
                 self.commit_apply(index);
             }
             let unstable = self.raft_log.unstable_entries().to_vec();

src/raft.rs
@@ -971,14 +971,26 @@ impl<T: Storage> Raft<T> {
         true
     }
 
-    /// Notifies that these raft logs have been well persisted.
+    /// Notifies that these raft logs or snapshot have been persisted.
     pub fn on_persist_entries(&mut self, index: u64, term: u64) {
         let update = self.raft_log.maybe_persist(index, term);
         if update && self.state == StateRole::Leader {
+            // Actually, if it is a leader and persisted index is updated, this term
+            // must be equal to self.term because the persisted index must be equal to
+            // the last index of entries from previous leader when it becomes leader
+            // (see the comments in become_leader), namely, the new persisted entries
+            // must come from this leader.
+            if term != self.term {
+                error!(
+                    self.logger,
+                    "leader's persisted index changed but the term {} is not the same as {}",
+                    term,
+                    self.term
+                );
+            }
             let self_id = self.id;
             let pr = self.mut_prs().get_mut(self_id).unwrap();
-            pr.maybe_update(index);
-            if self.maybe_commit() && self.should_bcast_commit() {
+            if pr.maybe_update(index) && self.maybe_commit() && self.should_bcast_commit() {
                 self.bcast_append();
             }
         }
@@ -1125,7 +1137,7 @@ impl<T: Storage> Raft<T> {
         self.state = StateRole::Leader;
 
         let last_index = self.raft_log.last_index();
-        // If there is only one peer, it becomes leader after starting
+        // If there is only one peer, it becomes leader after campaigning
         // so all logs must be persisted.
         // If not, it becomes leader after sending RequestVote msg.
         // Since all logs must be persisted before sending RequestVote

src/raft_log.rs
@@ -537,11 +537,16 @@ impl<T: Storage> RaftLog<T> {
         );
         let index = snapshot.get_metadata().index;
         assert!(index >= self.committed, "{} < {}", index, self.committed);
+        // If `persisted` is greater than `committed`, reset it to `committed`.
+        // It's because only the persisted entries whose index are less than `commited` can be
+        // considered the same as the data from snapshot.
+        // Although there may be some persisted entries with greater index are also committed,
+        // we can not judge them nor do we care about them because these entries can not be applied
+        // thus the invariant which is `applied` <= min(`persisted`, `committed`) is satisfied.
+        if self.persisted > self.committed {
+            self.persisted = self.committed;
+        }
         self.committed = index;
-        // persisted is just used for fetching committed entries.
-        // Here reset the persisted to index to satisfy its invariant which is
-        // persisted < unstable.offset and applied <= persisted.
-        self.persisted = index;
         self.unstable.restore(snapshot);
     }
 }
@@ -765,7 +770,7 @@ mod test {
         let mut raft_log = RaftLog::new(store, default_logger());
         raft_log.restore(new_snapshot(unstablesnapi, 1));
         assert_eq!(raft_log.committed, unstablesnapi);
-        assert_eq!(raft_log.persisted, unstablesnapi);
+        assert_eq!(raft_log.persisted, storagesnapi);
 
         let tests = vec![
             // cannot get term from storage
@@ -1521,7 +1526,7 @@ mod test {
         assert_eq!(raft_log.persisted, 100);
         raft_log.restore(new_snapshot(200, 1));
         assert_eq!(raft_log.committed, 200);
-        assert_eq!(raft_log.persisted, 200);
+        assert_eq!(raft_log.persisted, 100);
 
         for i in 201..210 {
             raft_log.append(&[new_entry(i, 1)]);
@@ -1536,13 +1541,12 @@ mod test {
         raft_log.stable_entries();
         raft_log.mut_store().wl().append(&unstable).expect("");
         raft_log.maybe_persist(209, 1);
-
         assert_eq!(raft_log.persisted, 209);
 
         raft_log.restore(new_snapshot(205, 1));
         assert_eq!(raft_log.committed, 205);
-        // persisted should backward to 205
-        assert_eq!(raft_log.persisted, 205);
+        // persisted should reset to previous commit index(200)
+        assert_eq!(raft_log.persisted, 200);
 
         // use smaller commit index, should panic
         assert!(

src/raw_node.rs
@@ -196,7 +196,8 @@ struct ReadyRecord {
     number: u64,
     // (index, term) of the last entry from the entries in Ready
     last_entry: Option<(u64, u64)>,
-    has_snapshot: bool,
+    // (index, term) of the snapshot in Ready
+    snapshot: Option<(u64, u64)>,
     messages: Vec<Message>,
 }
 
@@ -436,7 +437,7 @@ impl<T: Storage> RawNode<T> {
             // avoid out of order.
             for record in self.records.drain(..) {
                 assert_eq!(record.last_entry, None);
-                assert!(!record.has_snapshot);
+                assert_eq!(record.snapshot, None);
                 if !record.messages.is_empty() {
                     self.messages.push(record.messages);
                 }
@@ -472,7 +473,10 @@ impl<T: Storage> RawNode<T> {
                 "has snapshot but also has committed entries since {}",
                 self.commit_since_index
             );
-            rd_record.has_snapshot = true;
+            rd_record.snapshot = Some((
+                rd.snapshot.get_metadata().index,
+                rd.snapshot.get_metadata().term,
+            ));
             rd.must_sync = true;
         }
 
@@ -538,7 +542,7 @@ impl<T: Storage> RawNode<T> {
         let rd_record = self.records.back().unwrap();
         assert!(rd_record.number == rd.number);
         let raft = &mut self.raft;
-        if rd_record.has_snapshot {
+        if rd_record.snapshot.is_some() {
             raft.raft_log.stable_snap();
         }
         if rd_record.last_entry.is_some() {
@@ -564,6 +568,11 @@ impl<T: Storage> RawNode<T> {
                 break;
             }
             let mut record = self.records.pop_front().unwrap();
+
+            if let Some((i, t)) = record.snapshot {
+                index = i;
+                term = t;
+            }
 
             if let Some((i, t)) = record.last_entry {
                 index = i;

commit 7e7322b6afab1bb0b761c5b6b03aa4a6dea2addf

fix persisted index bug of snapshot (#417)
This PR fixes a bug introduced by https://github.com/tikv/raft-rs/pull/410.
Consider the case below
1. A receives a snapshot with index 10
2. A gets a ready and handles it asynchronously
3. A receives a new snapshot with index 20
4. A calls on_persist_ready for ready 1
In step 4, the persisted index can not be updated to 10 because the first_index has changed to 21 so the term check can not be passed. (details in `RaftLog::term`)
I add a `maybe_persist_snap` function to fix this problem. The snapshot does not need to check the term because its data must be committed before and can not be changed in future.

Signed-off-by: gengliqi <gengliqiii@gmail.com>

benches/suites/raw_node.rs
@@ -108,7 +108,7 @@ fn test_ready_raft_node(logger: &slog::Logger) -> RawNode<MemStorage> {
     node.raft.become_candidate();
     node.raft.become_leader();
     let unstable = node.raft.raft_log.unstable_entries().to_vec();
-    node.raft.raft_log.stable_entries();
+    node.raft.raft_log.stable_entries(1, 1);
     node.raft.raft_log.store.wl().append(&unstable).expect("");
     node.raft.on_persist_entries(1, 1);
     node.raft.commit_apply(1);
@@ -123,12 +123,10 @@ fn test_ready_raft_node(logger: &slog::Logger) -> RawNode<MemStorage> {
     }
     let _ = node.raft.append_entry(&mut entries);
     let unstable = node.raft.raft_log.unstable_entries().to_vec();
-    node.raft.raft_log.stable_entries();
+    node.raft.raft_log.stable_entries(101, 1);
     node.raft.raft_log.store.wl().append(&unstable).expect("");
-    node.raft.raft_log.stable_entries();
     // This increases 'committed_index' to `last_index` because there is only one node in quorum.
-    node.raft
-        .on_persist_entries(node.raft.raft_log.last_index(), 1);
+    node.raft.on_persist_entries(101, 1);
 
     let mut snap = Snapshot::default();
     snap.set_data(vec![0; 8 * 1024 * 1024]);

harness/src/interface.rs
@@ -58,19 +58,18 @@ impl Interface {
         if self.raft.is_some() {
             if let Some(snapshot) = self.raft_log.unstable_snapshot() {
                 let snap = snapshot.clone();
-                self.raft_log.stable_snap();
                 let index = snap.get_metadata().index;
-                let term = snap.get_metadata().term;
+                self.raft_log.stable_snap(index);
                 self.mut_store().wl().apply_snapshot(snap).expect("");
-                self.on_persist_entries(index, term);
+                self.on_persist_snap(index);
                 self.commit_apply(index);
             }
             let unstable = self.raft_log.unstable_entries().to_vec();
-            if !unstable.is_empty() {
-                self.raft_log.stable_entries();
-                let last_entry = unstable.last().unwrap();
+            if let Some(e) = unstable.last() {
+                let (last_idx, last_term) = (e.get_index(), e.get_term());
+                self.raft_log.stable_entries(last_idx, last_term);
                 self.mut_store().wl().append(&unstable).expect("");
-                self.on_persist_entries(last_entry.index, last_entry.term);
+                self.on_persist_entries(last_idx, last_term);
             }
         }
     }

src/log_unstable.rs
@@ -87,16 +87,51 @@ impl Unstable {
 
     /// Clears the unstable entries and moves the stable offset up to the
     /// last index, if there is any.
-    pub fn stable_entries(&mut self) {
+    pub fn stable_entries(&mut self, index: u64, term: u64) {
+        // The snapshot must be stabled before entries
+        assert!(self.snapshot.is_none());
         if let Some(entry) = self.entries.last() {
+            if entry.get_index() != index || entry.get_term() != term {
+                fatal!(
+                    self.logger,
+                    "the last one of unstable.slice has different index {} and term {}, expect {} {}",
+                    entry.get_index(),
+                    entry.get_term(),
+                    index,
+                    term
+                );
+            }
             self.offset = entry.get_index() + 1;
             self.entries.clear();
+        } else {
+            fatal!(
+                self.logger,
+                "unstable.slice is empty, expect its last one's index and term are {} and {}",
+                index,
+                term
+            );
         }
     }
 
     /// Clears the unstable snapshot.
-    pub fn stable_snap(&mut self) {
-        self.snapshot = None;
+    pub fn stable_snap(&mut self, index: u64) {
+        if let Some(snap) = &self.snapshot {
+            if snap.get_metadata().index != index {
+                fatal!(
+                    self.logger,
+                    "unstable.snap has different index {}, expect {}",
+                    snap.get_metadata().index,
+                    index
+                );
+            }
+            self.snapshot = None;
+        } else {
+            fatal!(
+                self.logger,
+                "unstable.snap is none, expect a snapshot with index {}",
+                index
+            );
+        }
     }
 
     /// From a given snapshot, restores the snapshot to self, but doesn't unpack.
@@ -319,7 +354,7 @@ mod test {
     }
 
     #[test]
-    fn test_stable_entries() {
+    fn test_stable_snapshot_and_entries() {
         let ents = vec![new_entry(5, 1), new_entry(5, 2), new_entry(6, 3)];
         let mut u = Unstable {
             entries: ents.clone(),
@@ -328,7 +363,8 @@ mod test {
             logger: crate::default_logger(),
         };
         assert_eq!(ents, u.entries);
-        u.stable_entries();
+        u.stable_snap(4);
+        u.stable_entries(6, 3);
         assert!(u.entries.is_empty());
         assert_eq!(u.offset, 7);
     }

src/raft.rs
@@ -971,7 +971,7 @@ impl<T: Storage> Raft<T> {
         true
     }
 
-    /// Notifies that these raft logs or snapshot have been persisted.
+    /// Notifies that these raft logs have been persisted.
     pub fn on_persist_entries(&mut self, index: u64, term: u64) {
         let update = self.raft_log.maybe_persist(index, term);
         if update && self.state == StateRole::Leader {
@@ -994,6 +994,11 @@ impl<T: Storage> Raft<T> {
                 self.bcast_append();
             }
         }
+    }
+
+    /// Notifies that the snapshot have been persisted.
+    pub fn on_persist_snap(&mut self, index: u64) {
+        self.raft_log.maybe_persist_snap(index);
     }
 
     /// Returns true to indicate that there will probably be some readiness need to be handled.

src/raft_log.rs
@@ -290,13 +290,13 @@ impl<T: Storage> RaftLog<T> {
 
     /// Clears the unstable entries and moves the stable offset up to the
     /// last index, if there is any.
-    pub fn stable_entries(&mut self) {
-        self.unstable.stable_entries();
+    pub fn stable_entries(&mut self, index: u64, term: u64) {
+        self.unstable.stable_entries(index, term);
     }
 
     /// Clears the unstable snapshot.
-    pub fn stable_snap(&mut self) {
-        self.unstable.stable_snap();
+    pub fn stable_snap(&mut self, index: u64) {
+        self.unstable.stable_snap(index);
     }
 
     /// Returns a reference to the unstable log.
@@ -461,15 +461,58 @@ impl<T: Storage> RaftLog<T> {
     /// Attempts to persist the index and term and returns whether it did.
     pub fn maybe_persist(&mut self, index: u64, term: u64) -> bool {
         // It's possible that the term check can be passed but index is greater
-        // than or equal to the unstable's offset in some corner cases.
-        // We handle these issues by not forwarding the persisted index. It's
-        // pretty intuitive because the offset means there are some entries whose
-        // index is greater than or equal to the offset has not been persisted yet.
+        // than or equal to the first_update_index in some corner cases.
+        // For example, there are 5 nodes, A B C D E.
+        // 1. A is leader and it proposes some raft logs but only B receives these logs.
+        // 2. B gets the Ready and the logs are persisted asynchronously.
+        // 2. A crashes and C becomes leader after getting the vote from D and E.
+        // 3. C proposes some raft logs and B receives these logs.
+        // 4. C crashes and A restarts and becomes leader again after getting the vote from D and E.
+        // 5. B receives the logs from A which are the same to the ones from step 1.
+        // 6. The logs from Ready has been persisted on B so it calls on_persist_ready and comes to here.
+        //
+        // We solve this problem by not forwarding the persisted index. It's pretty intuitive
+        // because the first_update_index means there are snapshot or some entries whose indexes
+        // are greater than or equal to the first_update_index have not been persisted yet.
+        let first_update_index = match &self.unstable.snapshot {
+            Some(s) => s.get_metadata().index,
+            None => self.unstable.offset,
+        };
         if index > self.persisted
-            && index < self.unstable.offset
-            && self.term(index).map_or(false, |t| t == term)
+            && index < first_update_index
+            && self.store.term(index).map_or(false, |t| t == term)
         {
             debug!(self.unstable.logger, "persisted index {}", index);
+            self.persisted = index;
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Attempts to persist the snapshot and returns whether it did.
+    pub fn maybe_persist_snap(&mut self, index: u64) -> bool {
+        if index > self.persisted {
+            // commit index should not be less than snapshot's index
+            if index > self.committed {
+                fatal!(
+                    self.unstable.logger,
+                    "snapshot's index {} > committed {}",
+                    index,
+                    self.committed,
+                )
+            }
+            // All of the indexes of later entries must be greater than snapshot's index
+            if index >= self.unstable.offset {
+                fatal!(
+                    self.unstable.logger,
+                    "snapshot's index {} >= offset {}",
+                    index,
+                    self.unstable.offset,
+                );
+            }
+
+            debug!(self.unstable.logger, "snapshot's persisted index {}", index);
             self.persisted = index;
             true
         } else {
@@ -917,8 +960,10 @@ mod test {
             assert_eq!(raft_log.persisted, snap_index);
             raft_log.append(new_ents);
             let unstable = raft_log.unstable_entries().to_vec();
-            raft_log.stable_entries();
-            raft_log.mut_store().wl().append(&unstable).expect("");
+            if let Some(e) = unstable.last() {
+                raft_log.stable_entries(e.get_index(), e.get_term());
+                raft_log.mut_store().wl().append(&unstable).expect("");
+            }
             let is_changed = raft_log.persisted != wpersist;
             assert_eq!(raft_log.maybe_persist(stablei, stablet), is_changed);
             if raft_log.persisted != wpersist {
@@ -963,7 +1008,9 @@ mod test {
             raft_log.append(&previous_ents[(unstable - 1)..]);
 
             let ents = raft_log.unstable_entries().to_vec();
-            raft_log.stable_entries();
+            if let Some(e) = ents.last() {
+                raft_log.stable_entries(e.get_index(), e.get_term());
+            }
             if &ents != wents {
                 panic!("#{}: unstableEnts = {:?}, want {:?}", i, ents, wents);
             }
@@ -1011,8 +1058,10 @@ mod test {
             let mut raft_log = RaftLog::new(store, l.clone());
             raft_log.append(&ents);
             let unstable = raft_log.unstable_entries().to_vec();
-            raft_log.stable_entries();
-            raft_log.mut_store().wl().append(&unstable).expect("");
+            if let Some(e) = unstable.last() {
+                raft_log.stable_entries(e.get_index(), e.get_term());
+                raft_log.mut_store().wl().append(&unstable).expect("");
+            }
             raft_log.maybe_persist(persisted, 1);
             assert_eq!(
                 persisted, raft_log.persisted,
@@ -1549,9 +1598,9 @@ mod test {
             .wl()
             .apply_snapshot(new_snapshot(200, 1))
             .expect("");
-        raft_log.stable_snap();
+        raft_log.stable_snap(200);
         let unstable = raft_log.unstable_entries().to_vec();
-        raft_log.stable_entries();
+        raft_log.stable_entries(209, 1);
         raft_log.mut_store().wl().append(&unstable).expect("");
         raft_log.maybe_persist(209, 1);
         assert_eq!(raft_log.persisted, 209);

src/raw_node.rs
@@ -542,11 +542,11 @@ impl<T: Storage> RawNode<T> {
         let rd_record = self.records.back().unwrap();
         assert!(rd_record.number == rd.number);
         let raft = &mut self.raft;
-        if rd_record.snapshot.is_some() {
-            raft.raft_log.stable_snap();
+        if let Some((index, _)) = rd_record.snapshot {
+            raft.raft_log.stable_snap(index);
         }
-        if rd_record.last_entry.is_some() {
-            raft.raft_log.stable_entries();
+        if let Some((index, term)) = rd_record.last_entry {
+            raft.raft_log.stable_entries(index, term);
         }
     }
 
@@ -563,15 +563,17 @@ impl<T: Storage> RawNode<T> {
     /// valid after ready being persisted.
     pub fn on_persist_ready(&mut self, number: u64) {
         let (mut index, mut term) = (0, 0);
+        let mut snap_index = 0;
         while let Some(record) = self.records.front() {
             if record.number > number {
                 break;
             }
             let mut record = self.records.pop_front().unwrap();
 
-            if let Some((i, t)) = record.snapshot {
-                index = i;
-                term = t;
+            if let Some((i, _)) = record.snapshot {
+                snap_index = i;
+                index = 0;
+                term = 0;
             }
 
             if let Some((i, t)) = record.last_entry {
@@ -583,7 +585,10 @@ impl<T: Storage> RawNode<T> {
                 self.messages.push(mem::take(&mut record.messages));
             }
         }
-        if term != 0 {
+        if snap_index != 0 {
+            self.raft.on_persist_snap(snap_index);
+        }
+        if index != 0 {
             self.raft.on_persist_entries(index, term);
         }
     }

harness/tests/integration_cases/test_raft.rs
@@ -101,10 +101,12 @@ fn voted_with_config(
 // Persist committed index and fetch next entries.
 fn next_ents(r: &mut Raft<MemStorage>, s: &MemStorage) -> Vec<Entry> {
     let unstable = r.raft_log.unstable_entries().to_vec();
-    r.raft_log.stable_entries();
-    s.wl().append(&unstable).expect("");
-    let (last_idx, last_term) = (r.raft_log.last_index(), r.raft_log.last_term());
-    r.on_persist_entries(last_idx, last_term);
+    if let Some(e) = unstable.last() {
+        let (last_idx, last_term) = (e.get_index(), e.get_term());
+        r.raft_log.stable_entries(last_idx, last_term);
+        s.wl().append(&unstable).expect("");
+        r.on_persist_entries(last_idx, last_term);
+    }
     let ents = r.raft_log.next_entries();
     r.commit_apply(r.raft_log.committed);
     ents.unwrap_or_else(Vec::new)

harness/tests/integration_cases/test_raft_paper.rs
@@ -35,12 +35,14 @@ pub fn commit_noop_entry(r: &mut Interface, s: &MemStorage) {
     // ignore further messages to refresh followers' commit index
     r.read_messages();
     let unstable = r.raft_log.unstable_entries().to_vec();
-    r.raft_log.stable_entries();
-    s.wl().append(&unstable).expect("");
-    let (last_index, last_term) = (r.raft_log.last_index(), r.raft_log.last_term());
-    r.on_persist_entries(last_index, last_term);
-    let committed = r.raft_log.committed;
-    r.commit_apply(committed);
+    if let Some(e) = unstable.last() {
+        let (last_idx, last_term) = (e.get_index(), e.get_term());
+        r.raft_log.stable_entries(last_idx, last_term);
+        s.wl().append(&unstable).expect("");
+        r.on_persist_entries(last_idx, last_term);
+        let committed = r.raft_log.committed;
+        r.commit_apply(committed);
+    }
 }
 
 fn accept_and_reply(m: &Message) -> Message {

harness/tests/integration_cases/test_raw_node.rs
@@ -799,6 +799,8 @@ fn test_bounded_uncommitted_entries_growth_with_partition() {
     raw_node.campaign().unwrap();
     loop {
         let rd = raw_node.ready();
+        s.wl().set_hardstate(rd.hs().unwrap().clone());
+        s.wl().append(rd.entries()).unwrap();
         if rd
             .ss()
             .map_or(false, |ss| ss.leader_id == raw_node.raft.leader_id)
@@ -1460,8 +1462,92 @@ fn test_async_ready_become_leader() {
     }
     assert_eq!(count, 4);
 
+    s.wl().append(rd.entries()).unwrap();
+
     let light_rd = raw_node.advance_append(rd);
     assert_eq!(light_rd.commit_index(), None);
     assert!(light_rd.committed_entries().is_empty());
     assert!(light_rd.messages().is_empty());
+}
+
+#[test]
+fn test_async_ready_multiple_snapshot() {
+    let l = default_logger();
+    let s = new_storage();
+    s.wl()
+        .apply_snapshot(new_snapshot(1, 1, vec![1, 2]))
+        .unwrap();
+
+    let mut raw_node = new_raw_node(1, vec![1, 2], 10, 1, s.clone(), &l);
+
+    let snapshot = new_snapshot(10, 2, vec![1, 2]);
+    let mut snapshot_msg = new_message(2, 1, MessageType::MsgSnapshot, 0);
+    snapshot_msg.set_term(2);
+    snapshot_msg.set_snapshot(snapshot.clone());
+    raw_node.step(snapshot_msg).unwrap();
+
+    let mut entries = vec![];
+    for i in 11..14 {
+        entries.push(new_entry(2, i, Some("hello")));
+    }
+    let mut append_msg = new_message_with_entries(2, 1, MessageType::MsgAppend, entries.to_vec());
+    append_msg.set_term(2);
+    append_msg.set_index(10);
+    append_msg.set_log_term(2);
+    append_msg.set_commit(12);
+    raw_node.step(append_msg).unwrap();
+
+    let rd = raw_node.ready();
+    assert_eq!(rd.number(), 1);
+    // If there is a snapshot, the committed entries should be empty.
+    must_cmp_ready(
+        &rd,
+        &Some(soft_state(2, StateRole::Follower)),
+        &Some(hard_state(2, 12, 0)),
+        &entries,
+        &[],
+        &Some(snapshot),
+        true,
+        true,
+    );
+    s.wl().set_hardstate(rd.hs().unwrap().clone());
+    s.wl().apply_snapshot(rd.snapshot().clone()).unwrap();
+    s.wl().append(rd.entries()).unwrap();
+
+    raw_node.advance_append_async(rd);
+
+    let snapshot = new_snapshot(20, 1, vec![1, 2]);
+    let mut snapshot_msg = new_message(2, 1, MessageType::MsgSnapshot, 0);
+    snapshot_msg.set_term(2);
+    snapshot_msg.set_snapshot(snapshot.clone());
+    raw_node.step(snapshot_msg).unwrap();
+
+    raw_node.on_persist_ready(1);
+
+    assert_eq!(raw_node.raft.raft_log.persisted, 13);
+
+    raw_node.advance_apply_to(10);
+
+    let rd = raw_node.ready();
+    assert_eq!(rd.number(), 2);
+    must_cmp_ready(
+        &rd,
+        &None,
+        &Some(hard_state(2, 20, 0)),
+        &[],
+        &[],
+        &Some(snapshot),
+        // the msg from ready 1
+        false,
+        true,
+    );
+    s.wl().set_hardstate(rd.hs().unwrap().clone());
+    s.wl().apply_snapshot(rd.snapshot().clone()).unwrap();
+
+    let light_rd = raw_node.advance_append(rd);
+    assert_eq!(light_rd.commit_index(), None);
+    assert!(light_rd.committed_entries().is_empty());
+    assert!(!light_rd.messages().is_empty());
+
+    raw_node.advance_apply_to(20);
 }