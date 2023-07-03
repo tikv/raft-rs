9db903b3f50183753c1b9802e134d44153949886
GuyLewin <Guy@lewin.co.il>
2023-07-02 21:25:42 -0400

    feature: validate term value
    Signed-off-by: GuyLewin <Guy@lewin.co.il>

--- src/raft.rs
+++ src/raft.rs
@@ -380,6 +380,12 @@ impl<T: Storage> Raft<T> {
         if raft_state.hard_state != HardState::default() {
             r.load_state(&raft_state.hard_state);
         }
+        if r.term == 0 {
+            fatal!(
+                r.logger,
+                "Invalid term value: 0"
+            );
+        }
         if c.applied > 0 {
             r.commit_apply(c.applied);
         }
